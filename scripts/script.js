f4623c8f554c80d750f3f1910b91a24bcaa2153e
Alex Hand <alex.j.hand@gmail.com>
2017-09-03 19:32:01 -0500

    Added form and ability to capture input

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,36 @@
-console.log("js");
-$(document).ready(console.log('JQ'));
+//console.log("js");
+//$(document).ready(console.log('JQ'));
+
+// Global variables
+var employees = [];
+
+$(document).ready(pageReady);
+
+function pageReady() {
+    $('#submitButton').on('click', addEmployee); // Need to add function
+}
+
+function addEmployee() { 
+    // Retrieve employee info from form
+    // Use info from form to create Employee object
+    new Employee($('#firstNameIn').val(), $('#lastNameIn').val(), $('#idIn').val(),$('#titleIn').val(),$('#salaryIn').val());
+    // Reset form
+    $('#firstNameIn').val('');
+    $('#lastNameIn').val('');
+    $('#idIn').val('');
+    $('#titleIn').val('');
+    $('#salaryIn').val('');
+ }
+
+// Employee object constructor
+function Employee(firstNameIn, lastNameIn, idIn, titleIn, salaryIn) {
+    this.firstName = firstNameIn;
+    this.lastName = lastNameIn;
+    this.id = idIn;
+    this.title = titleIn;
+    this.salary = salaryIn;
+
+    // Once employee is created, add them to employees array
+    employees.push(this);
+    console.log(this);
+}
